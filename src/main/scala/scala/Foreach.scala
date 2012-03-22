1ff3a479651d48eed12685d9e8b5bac384746338
Aleksandar Prokopec <axel22@gmail.com>
2012-03-22 19:59:34 +0100

    A corresponding while benchmark the performance of which we expect after inlining.

--- src/main/scala/scala/Foreach.scala
+++ src/main/scala/scala/Foreach.scala
@@ -4,14 +4,37 @@ package scala
 
 
 
+object ForeachConfig {
+  val size = 10000
+}
+
+
 object Foreach extends Benchmark {
   
   var globalsum = 0
   
   def snippet() {
     var sum = 0
-    for (i <- 0 until 10000) {
+    for (i <- 0 until ForeachConfig.size) {
       sum += i
+    }
+    globalsum = sum
+  }
+  
+}
+
+
+object While extends Benchmark {
+  
+  var globalsum = 0
+  
+  def snippet() {
+    var sum = 0
+    var i = 0
+    val limit = ForeachConfig.size
+    while (i < limit) {
+      sum += i
+      i += 1
     }
     globalsum = sum
   }
