b49b5163b462012301d14570c88c138f95e374e8
Aleksandar Prokopec <axel22@gmail.com>
2012-03-22 20:31:36 +0100

    Generic array access.

---
+++ src/main/scala/scala/ArrayAccess.scala
@@ -0,0 +1,61 @@
+package scala
+
+
+
+
+
+
+object ArrayAccessConfig {
+  val size = 10000
+}
+
+
+object ArrayAccessGeneric extends Benchmark {
+  
+  var globalsum = 0
+  val array = new Array[AnyRef](ArrayAccessConfig.size)
+  
+  def snippet() {
+    accessloop(array)
+  }
+  
+  def accessloop[T](array: Array[T]) {
+    var i = 0
+    var sum = 0
+    val limit = ArrayAccessConfig.size
+    while (i < limit) {
+      if (array(i) != null) sum = 1
+      i += 1
+    }
+    globalsum = sum
+  }
+  
+}
+
+
+object ArrayAccessSpecial extends Benchmark {
+  
+  var globalsum = 0
+  val array = new Array[AnyRef](ArrayAccessConfig.size)
+  
+  def snippet() {
+    accessloop(array)
+  }
+  
+  def accessloop(array: Array[AnyRef]) {
+    var i = 0
+    var sum = 0
+    val limit = ArrayAccessConfig.size
+    while (i < limit) {
+      if (array(i) != null) sum = 1
+      i += 1
+    }
+    globalsum = sum
+  }
+  
+}
+
+
+
+
+
